f8b8bea5b7b01ead937421c5df7c2b16809179b0
Andrew Arrow <oneone@gmail.com>
2021-03-05 14:45:07 -0800

    percent_s

--- draw.go
+++ draw.go
@@ -32,50 +32,36 @@ func StartWithDrawing() {
 		time.Sleep(time.Second * 1)
 	}
 }
+func DrawWithParams(ab string) {
+	draw := `
+           +                 +
+          / \               / \
+         /   \             /   \
+        /     \           /     \
+       /       \         /       \
+      +         \       /         +
+       \         \     /         /  
+        \         \   /         /  
+         \         \ /         /  
+          \         %s         /
+           \       / \       /
+            \     /   \     /
+             \   /     \   /
+              \ /       \ /
+               +         +
+`
+	fmt.Printf("\n\n\n%s\n\n\n", fmt.Sprintf(draw, ab))
+}
 func (t *Tron) PingDraw(from3D *ThreeD) []int {
 	list := []int{}
 	if t.Name == "zero" {
 		//fmt.Printf("The SHARED ZERO hit by %s\n", from3D.Name)
 		list = cacheTimes
 		if from3D.Name == "a" {
-			draw := `
-           +                 +
-          / \               / \
-         /   \             /   \
-        /     \           /     \
-       /       \         /       \
-      +         \       /         +
-       \         \     /         /  
-        \         \   /         /  
-         \         \ /         /  
-          \         a         /
-           \       / \       /
-            \     /   \     /
-             \   /     \   /
-              \ /       \ /
-               +         +
-`
-			fmt.Printf("\n\n\n%s\n\n\n", draw)
+			DrawWithParams("a")
 			return list
 		} else {
-			draw := `
-           +                 +
-          / \               / \
-         /   \             /   \
-        /     \           /     \
-       /       \         /       \
-      +         \       /         +
-       \         \     /         /  
-        \         \   /         /  
-         \         \ /         /  
-          \         b         /
-           \       / \       /
-            \     /   \     /
-             \   /     \   /
-              \ /       \ /
-               +         +
-`
-			fmt.Printf("\n\n\n%s\n\n\n", draw)
+			DrawWithParams("b")
 			return list
 		}
 	} else {
