0ab3eaed38bf4fe668244dfd34cd2cb7cbcbf528
Andrew Arrow <oneone@gmail.com>
2021-03-06 15:39:43 -0800

    doublin

--- math/main.go
+++ math/main.go
@@ -10,6 +10,9 @@ import (
 var lockMF sync.Mutex = sync.Mutex{}
 var fourCrank chan bool = make(chan bool, 1024)
 var uniCrank chan bool = make(chan bool, 1024)
+var uniSeed = 9
+var mfSeed = 3
+var usSeed = 1
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
@@ -26,14 +29,15 @@ func three() {
 		list := []int{1, 2, 4, 0, 8, 7, 5, 0, -1, -2, -4, 0, -8, -7, -5, 0}
 		for _, num := range list {
 			if num == 8 || num == 7 || num == 5 {
-				fmt.Println(loops, "LEFT", num)
+				fmt.Println(usSeed, loops, "LEFT", num)
 			} else if num == 0 {
-				fmt.Println(loops, "ZERO", num)
+				fmt.Println(usSeed, loops, "ZERO", num)
 			} else {
-				fmt.Println(loops, "RIGHT", num)
+				fmt.Println(usSeed, loops, "RIGHT", num)
 			}
 		}
 		loops++
+		usSeed = usSeed * 2
 	}
 }
 func maleFemale() {
@@ -46,9 +50,10 @@ func maleFemale() {
 				lockMF.Unlock()
 				continue
 			}
-			fmt.Println("                  ", loops, num)
+			fmt.Println("                  ", mfSeed, loops, num)
 		}
 		loops++
+		mfSeed = mfSeed * 2
 		fourCrank <- true
 	}
 }
@@ -58,10 +63,11 @@ func theUni() {
 		list := []int{9}
 		for _, num := range list {
 			lockMF.Lock()
-			fmt.Println("999999999999999999              ", loops, num)
+			fmt.Println("                                 ", uniSeed, loops, num)
 			lockMF.Unlock()
 		}
 		loops++
+		uniSeed = uniSeed * 2
 		uniCrank <- true
 		time.Sleep(time.Second)
 	}
