5e433850b169d701644ce1206312e227c29135a8
Andrew Arrow <oneone@gmail.com>
2021-03-06 19:46:25 -0800

    on off and 1 more state

--- virtual/main.go
+++ virtual/main.go
@@ -30,14 +30,14 @@ func main() {
 	fmt.Println(fb)
 
 	a = FancyBit{1}
-	c = FancyBit{0}
-	d = FancyBit{2}
-	e := FancyBit{3}
-	f := FancyBit{4}
-	g := FancyBit{5}
-	h := FancyBit{6}
-	i := FancyBit{7}
-	j := FancyBit{8}
+	b = FancyBit{0}
+	c = FancyBit{2}
+	d = FancyBit{3}
+	e := FancyBit{4}
+	f := FancyBit{5}
+	g := FancyBit{6}
+	h := FancyBit{7}
+	i := FancyBit{8}
 	fb = FancyByte{}
 	fb.List = append(fb.List, a)
 	fb.List = append(fb.List, b)
@@ -48,7 +48,6 @@ func main() {
 	fb.List = append(fb.List, g)
 	fb.List = append(fb.List, h)
 	fb.List = append(fb.List, i)
-	fb.List = append(fb.List, j)
 
 	fb.Base9()
 	fmt.Println(fb)
