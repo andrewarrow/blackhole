9cde2d8ab3fc33f4f8d76a1d794f87aa88ddc317
Andrew Arrow <oneone@gmail.com>
2021-03-07 07:55:01 -0800

    shapes

--- virtual/main.go
+++ virtual/main.go
@@ -14,35 +14,38 @@ func main() {
 	fb.List = append(fb.List, c)
 
 	fb.Base2()
-	fmt.Println(fb.Base2Add(&fb))
+	//fmt.Println(fb.Base2Add(&fb))
 
-	fb = MakeBase3FancyByte("3936936393363")
-	fb.Base3()
-	fmt.Println(fb)
-	fb = MakeBase3FancyByte("9366639393936")
-	fb.Base3()
-	fmt.Println(fb)
+	fb1 := MakeBase3FancyByte("3936936393363")
+	fb1.Base3()
+	fmt.Println(fb1)
+	fb2 := MakeBase3FancyByte("9366639393936")
+	fb2.Base3()
+	fmt.Println(fb2)
+	sum := fb1.Base3Add(&fb2)
+	sum.Base3()
 
-	a = FancyBit{1}
-	b = FancyBit{0}
-	c = FancyBit{2}
-	d := FancyBit{3}
-	e := FancyBit{4}
-	f := FancyBit{5}
-	g := FancyBit{6}
-	h := FancyBit{7}
-	i := FancyBit{8}
-	fb = FancyByte{}
-	fb.List = append(fb.List, a)
-	fb.List = append(fb.List, b)
-	fb.List = append(fb.List, c)
-	fb.List = append(fb.List, d)
-	fb.List = append(fb.List, e)
-	fb.List = append(fb.List, f)
-	fb.List = append(fb.List, g)
-	fb.List = append(fb.List, h)
-	fb.List = append(fb.List, i)
+	/*
+		a = FancyBit{1}
+		b = FancyBit{0}
+		c = FancyBit{2}
+		d := FancyBit{3}
+		e := FancyBit{4}
+		f := FancyBit{5}
+		g := FancyBit{6}
+		h := FancyBit{7}
+		i := FancyBit{8}
+		fb = FancyByte{}
+		fb.List = append(fb.List, a)
+		fb.List = append(fb.List, b)
+		fb.List = append(fb.List, c)
+		fb.List = append(fb.List, d)
+		fb.List = append(fb.List, e)
+		fb.List = append(fb.List, f)
+		fb.List = append(fb.List, g)
+		fb.List = append(fb.List, h)
+		fb.List = append(fb.List, i)
 
-	fb.Base9()
-	fmt.Println(fb)
+		fb.Base9()
+		//fmt.Println(fb)*/
 }
--- virtual/rules.go
+++ virtual/rules.go
@@ -44,31 +44,42 @@ func (fb *FancyByte) Base3Add(target *FancyByte) *FancyByte {
 	// 3936936393363
 	// 9366639393936
 	// =
-	// 1111100111110
-	a1 := FancyBit{1}
-	a0 := FancyBit{0}
+	// 9966969696966
+	a9 := FancyBit{9}
+	a3 := FancyBit{3}
+	a6 := FancyBit{6}
 	carry := false
 	for i, n := range fb.List {
 		if carry {
-			result.List = append([]FancyBit{a1}, result.List...)
+			result.List = append([]FancyBit{a9}, result.List...)
 			carry = false
 			continue
 		}
 		other := target.List[i].Val
-		if other == 1 && n.Val == 1 {
+		if other == 9 && n.Val == 9 {
 			carry = true
-			result.List = append([]FancyBit{a0}, result.List...)
-		} else if other == 1 && n.Val == 0 {
-			result.List = append([]FancyBit{a1}, result.List...)
-		} else if other == 0 && n.Val == 1 {
-			result.List = append([]FancyBit{a1}, result.List...)
-		} else if other == 0 && n.Val == 0 {
-			result.List = append([]FancyBit{a0}, result.List...)
+			result.List = append([]FancyBit{a9}, result.List...)
+		} else if other == 3 && n.Val == 3 {
+			result.List = append([]FancyBit{a3}, result.List...)
+		} else if other == 6 && n.Val == 6 {
+			result.List = append([]FancyBit{a6}, result.List...)
+		} else if other == 6 && n.Val == 3 {
+			result.List = append([]FancyBit{a6}, result.List...)
+		} else if other == 3 && n.Val == 6 {
+			result.List = append([]FancyBit{a6}, result.List...)
+		} else if other == 9 && n.Val == 6 {
+			result.List = append([]FancyBit{a9}, result.List...)
+		} else if other == 6 && n.Val == 9 {
+			result.List = append([]FancyBit{a9}, result.List...)
+		} else if other == 3 && n.Val == 9 {
+			result.List = append([]FancyBit{a9}, result.List...)
+		} else if other == 9 && n.Val == 3 {
+			result.List = append([]FancyBit{a9}, result.List...)
 		}
 		fmt.Println(other, n)
 	}
 	if carry {
-		result.List = append([]FancyBit{a1}, result.List...)
+		result.List = append([]FancyBit{a9}, result.List...)
 	}
 
 	return &result
